8bb973c459c91d272e7d8e7f9b787543b99d801c
cleitonrs <rscleitonrs@gmail.com>
2022-10-12 13:41:39 -0300

    text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Text } from './components/Text'
 import './styles/global.css'
 
 export function App() {
 
 
   return (
-    <h1 className='text-green-500'>Hello World!</h1>
+    <Text />
   )
 }
 
